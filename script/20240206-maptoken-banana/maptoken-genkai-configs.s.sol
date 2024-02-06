2d885a8819cf1df7580b8ede7aa0114288b2b79c
nxqbao <quocbao300@gmail.com>
2024-02-06 14:10:12 +0700

    proposal: mapping token for banana, vx, genkai and change stable node GV

---
+++ script/20240206-maptoken-banana/maptoken-genkai-configs.s.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Migration__MapToken_Genkai_Config {
+  address constant _genkaiRoninToken = address(0x1F7c16FCe4fC894143aFB5545Bf04f676bf7DCf3);
+  address constant _genkaiMainchainToken = address(0x1F7c16FCe4fC894143aFB5545Bf04f676bf7DCf3);
+}
