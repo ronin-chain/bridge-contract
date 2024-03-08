fefb613b7b75bcb88d81350c633b3b3f9a57024f
nxqbao <quocbao300@gmail.com>
2024-03-08 15:00:15 +0700

    script: init commit

---
+++ script/20240308-maptoken-aperios/caller-configs.s.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Migration__Caller_Config {
+  address internal _governor = 0xe880802580a1fbdeF67ACe39D1B21c5b2C74f059; // TODO: replace by address of the SV governor
+}
