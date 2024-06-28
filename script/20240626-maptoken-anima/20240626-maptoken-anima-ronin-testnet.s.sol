b1ff39eefea7185b95acc757cc087ad7128346d8
tringuyenskymavis <tri.minh.nguyen@skymavis.com>
2024-06-28 10:29:34 +0700

    chore: refactor code

--- script/20240626-maptoken-anima/20240626-maptoken-anima-ronin-testnet.s.sol
+++ script/20240626-maptoken-anima/20240626-maptoken-anima-ronin-testnet.s.sol
@@ -7,48 +7,21 @@ import "./base-maptoken.s.sol";
 import { Proposal } from "@ronin/contracts/libraries/Proposal.sol";
 
 contract Migration__20242606_MapTokenAnimaRoninTestnet is Base__MapToken, Factory__MapTokensRoninchain {
+  function setUp() public virtual override {
+    super.setUp();
+    _governors = new address[](4);
+    _governors[3] = 0xd24D87DDc1917165435b306aAC68D99e0F49A3Fa;
+    _governors[2] = 0xb033ba62EC622dC54D0ABFE0254e79692147CA26;
+    _governors[0] = 0x087D08e3ba42e64E3948962dd1371F906D1278b9;
+    _governors[1] = 0x52ec2e6BBcE45AfFF8955Da6410bb13812F4289F;
+  }
+
   function _initCaller() internal override(Base__MapToken, Factory__MapTokensRoninchain) returns (address) {
     return Base__MapToken._initCaller();
   }
 
   function _initTokenList() internal override(Base__MapToken, Factory__MapTokensRoninchain) returns (uint256 totalToken, MapTokenInfo[] memory infos) {
     return Base__MapToken._initTokenList();
-  }
-
-  function _verifyAndExecuteProposal() internal virtual override {
-    // ================ VERIFY AND EXECUTE PROPOSAL ===============
-    // LibProposal.verifyProposalGasAmount(address(_roninBridgeManager), targets, values, calldatas, gasAmounts);
-    (address[] memory targets, uint256[] memory values, bytes[] memory calldatas, uint256[] memory gasAmounts) = _prepareProposal();
-
-    uint256 expiredTime = block.timestamp + 14 days;
-
-    Proposal.ProposalDetail memory proposal = Proposal.ProposalDetail({
-      nonce: 2,
-      chainId: block.chainid,
-      expiryTimestamp: expiredTime,
-      executor: 0x0000000000000000000000000000000000000000,
-      targets: targets,
-      values: values,
-      calldatas: calldatas,
-      gasAmounts: gasAmounts
-    });
-
-    address[] memory governors = new address[](4);
-    governors[3] = 0xd24D87DDc1917165435b306aAC68D99e0F49A3Fa;
-    governors[2] = 0xb033ba62EC622dC54D0ABFE0254e79692147CA26;
-    governors[0] = 0x087D08e3ba42e64E3948962dd1371F906D1278b9;
-    governors[1] = 0x52ec2e6BBcE45AfFF8955Da6410bb13812F4289F;
-
-    // _verifyRoninProposalGasAmount(targets, values, calldatas, gasAmounts);
-
-    vm.broadcast(governors[0]);
-    _roninBridgeManager.propose(block.chainid, expiredTime, address(0), targets, values, calldatas, gasAmounts);
-    vm.broadcast(governors[0]);
-    _roninBridgeManager.castProposalVoteForCurrentNetwork(proposal, Ballot.VoteType.For);
-    vm.broadcast(governors[1]);
-    _roninBridgeManager.castProposalVoteForCurrentNetwork(proposal, Ballot.VoteType.For);
-    vm.broadcast(governors[2]);
-    _roninBridgeManager.castProposalVoteForCurrentNetwork(proposal, Ballot.VoteType.For);
   }
 
   function run() public override {
--- script/factories/factory-maptoken-mainchain.s.sol
+++ script/factories/factory-maptoken-mainchain.s.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
+import "@ronin/contracts/libraries/Ballot.sol";
 import { console2 } from "forge-std/console2.sol";
 import { StdStyle } from "forge-std/StdStyle.sol";
 import { RoninBridgeManager } from "@ronin/contracts/ronin/gateway/RoninBridgeManager.sol";
 import { IMainchainGatewayV3 } from "@ronin/contracts/interfaces/IMainchainGatewayV3.sol";
 import { GlobalProposal } from "@ronin/contracts/libraries/GlobalProposal.sol";
+import { Proposal } from "@ronin/contracts/libraries/Proposal.sol";
 import { LibTokenInfo, TokenStandard } from "@ronin/contracts/libraries/LibTokenInfo.sol";
 import { Contract } from "../utils/Contract.sol";
 import { Migration } from "../Migration.s.sol";
-import { Network } from "../utils/Network.sol";
+import { Network, TNetwork } from "../utils/Network.sol";
 import { Contract } from "../utils/Contract.sol";
 import { IGeneralConfigExtended } from "../interfaces/IGeneralConfigExtended.sol";
-
+import { SignatureConsumer } from "@ronin/contracts/interfaces/consumers/SignatureConsumer.sol";
 import { MapTokenInfo } from "../libraries/MapTokenInfo.sol";
 import { LibCompanionNetwork } from "script/shared/libraries/LibCompanionNetwork.sol";
+import { MainchainBridgeAdminUtils } from "test/helpers/MainchainBridgeAdminUtils.t.sol";
+import { MainchainBridgeManager } from "@ronin/contracts/mainchain/MainchainBridgeManager.sol";
+import { LibProposal } from "script/shared/libraries/LibProposal.sol";
 
 abstract contract Factory__MapTokensMainchain is Migration {
   using LibCompanionNetwork for *;
@@ -22,7 +27,9 @@ abstract contract Factory__MapTokensMainchain is Migration {
   RoninBridgeManager internal _roninBridgeManager;
   address internal _mainchainGatewayV3;
   address internal _mainchainBridgeManager;
-  address private _governor;
+  address internal _governor;
+  address[] internal _governors;
+  uint256[] internal _governorPKs;
 
   function setUp() public virtual override {
     super.setUp();
@@ -34,8 +41,79 @@ abstract contract Factory__MapTokensMainchain is Migration {
     _governor = _initCaller();
   }
 
+  function run() public virtual {
+    _proposeProposal();
+  }
+
   function _initCaller() internal virtual returns (address);
   function _initTokenList() internal virtual returns (uint256 totalToken, MapTokenInfo[] memory infos);
+
+  function _proposeProposal() internal virtual {
+    Proposal.ProposalDetail memory proposal = _createAndVerifyProposal();
+
+    vm.broadcast(_governor);
+    _roninBridgeManager.propose(
+      proposal.chainId, proposal.expiryTimestamp, proposal.executor, proposal.targets, proposal.values, proposal.calldatas, proposal.gasAmounts
+    );
+  }
+
+  function _relayProposal() internal {
+    Proposal.ProposalDetail memory proposal = _createAndVerifyProposal();
+
+    MainchainBridgeAdminUtils mainchainProposalUtils =
+      new MainchainBridgeAdminUtils(2021, _governorPKs, MainchainBridgeManager(_mainchainBridgeManager), _governors[0]);
+
+    Ballot.VoteType[] memory supports_ = new Ballot.VoteType[](_governors.length);
+    require(_governors.length > 0 && _governors.length == _governorPKs.length, "Invalid governors information");
+
+    for (uint256 i; i < _governors.length; ++i) {
+      supports_[i] = Ballot.VoteType.For;
+    }
+
+    SignatureConsumer.Signature[] memory signatures = mainchainProposalUtils.generateSignatures(proposal, _governorPKs);
+    vm.broadcast(_governors[0]);
+
+    MainchainBridgeManager(_mainchainBridgeManager).relayProposal{ gas: 2_000_000 }(proposal, supports_, signatures);
+  }
+
+  function _createAndVerifyProposal() internal returns (Proposal.ProposalDetail memory proposal) {
+    (address[] memory mainchainTokens, address[] memory roninTokens, TokenStandard[] memory standards, uint256[][4] memory thresholds) =
+      _prepareMapTokensAndThresholds();
+    bytes memory innerData = abi.encodeCall(IMainchainGatewayV3.mapTokensAndThresholds, (mainchainTokens, roninTokens, standards, thresholds));
+    bytes memory proxyData = abi.encodeWithSignature("functionDelegateCall(bytes)", innerData);
+
+    uint256 expiredTime = block.timestamp + 14 days;
+    address[] memory targets = new address[](1);
+    uint256[] memory values = new uint256[](1);
+    bytes[] memory calldatas = new bytes[](1);
+    uint256[] memory gasAmounts = new uint256[](1);
+
+    targets[0] = _mainchainGatewayV3;
+    values[0] = 0;
+    calldatas[0] = proxyData;
+    gasAmounts[0] = 1_000_000;
+
+    uint256 nonce = (block.chainid == 2021 || block.chainid == 2020) ? 0 : MainchainBridgeManager(_mainchainBridgeManager).round(11155111) + 1;
+
+    // Verify gas when call from mainchain.
+    LibProposal.verifyProposalGasAmount(address(_mainchainBridgeManager), targets, values, calldatas, gasAmounts);
+
+    // // Verify gas when call from ronin.
+    // (uint256 companionChainId, TNetwork companionNetwork) = network().companionNetworkData();
+    // address companionManager = config.getAddress(companionNetwork, Contract.MainchainBridgeManager.key());
+    // LibProposal.verifyMainchainProposalGasAmount(companionNetwork, companionManager, targets, values, calldatas, gasAmounts);
+
+    proposal = Proposal.ProposalDetail({
+      nonce: nonce,
+      chainId: block.chainid,
+      expiryTimestamp: expiredTime,
+      executor: address(0),
+      targets: targets,
+      values: values,
+      calldatas: calldatas,
+      gasAmounts: gasAmounts
+    });
+  }
 
   function _prepareMapTokensAndThresholds()
     internal
@@ -69,34 +147,31 @@ abstract contract Factory__MapTokensMainchain is Migration {
     }
   }
 
-  function _prepareProposal() internal returns (address[] memory targets, uint256[] memory values, bytes[] memory calldatas, uint256[] memory gasAmounts) {
-    (address[] memory mainchainTokens, address[] memory roninTokens, TokenStandard[] memory standards, uint256[][4] memory thresholds) =
-      _prepareMapTokensAndThresholds();
-    bytes memory innerData = abi.encodeCall(IMainchainGatewayV3.mapTokensAndThresholds, (mainchainTokens, roninTokens, standards, thresholds));
-    bytes memory proxyData = abi.encodeWithSignature("functionDelegateCall(bytes)", innerData);
+  function _cheatStorage(address[] memory governors) internal {
+    // ================ Cheat Storage ===============
 
-    targets = new address[](1);
-    values = new uint256[](1);
-    calldatas = new bytes[](1);
-    gasAmounts = new uint256[](1);
+    bytes32 governorsSlot = keccak256(abi.encode(0xc648703095712c0419b6431ae642c061f0a105ac2d7c3d9604061ef4ebc38300));
+    uint256 length = governors.length;
+    //cheat governors addresses
+    for (uint256 i; i < length; ++i) {
+      bytes32 governorSlotId = bytes32(uint256(governorsSlot) + uint256(i));
+      vm.store(_mainchainBridgeManager, governorSlotId, bytes32(uint256(uint160(governors[i]))));
+    }
+    //after cheat
+    for (uint256 i; i < length; ++i) {
+      bytes32 governorSlotId = bytes32(uint256(governorsSlot) + uint256(i));
+      bytes32 afterCheatData = vm.load(_mainchainBridgeManager, bytes32(uint256(governorsSlot) + uint256(i)));
 
-    targets[0] = _mainchainGatewayV3;
-    values[0] = 0;
-    calldatas[0] = proxyData;
-    gasAmounts[0] = 1_000_000;
-  }
+      assertEq(afterCheatData, bytes32(uint256(uint160(governors[i]))));
+    }
 
-  function _verifyAndExecuteProposal() internal virtual {
-    (address[] memory targets, uint256[] memory values, bytes[] memory calldatas, uint256[] memory gasAmounts) = _prepareProposal();
-
-    uint256 chainId = network().companionChainId();
-    uint256 expiredTime = block.timestamp + 14 days;
-
-    vm.broadcast(_governor);
-    _roninBridgeManager.propose(chainId, expiredTime, address(0), targets, values, calldatas, gasAmounts);
-  }
-
-  function run() public virtual {
-    _verifyAndExecuteProposal();
+    //cheat governors weights
+    bytes32 governorsWeightSlot = bytes32(uint256(0xc648703095712c0419b6431ae642c061f0a105ac2d7c3d9604061ef4ebc38300) + uint256(2));
+    for (uint256 i; i < length; ++i) {
+      address key = governors[i];
+      bytes32 valueSlot = keccak256(abi.encode(key, governorsWeightSlot));
+      vm.store(_mainchainBridgeManager, valueSlot, bytes32(uint256(uint96(100))));
+    }
+    bytes32 governorsWeightData = vm.load(_mainchainBridgeManager, keccak256(abi.encode(0x087D08e3ba42e64E3948962dd1371F906D1278b9, governorsWeightSlot)));
   }
 }
