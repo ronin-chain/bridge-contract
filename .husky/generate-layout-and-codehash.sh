9bbf36ed4302b1a9d852a029df03c69703905898
TuDo1403 <tudo.dev@gmail.com>
2025-04-08 17:33:38 +0700

    chore: zeroize metadata before calculating codehash

--- .husky/generate-layout-and-codehash.sh
+++ .husky/generate-layout-and-codehash.sh
@@ -10,43 +10,50 @@ mkdir -p logs/storage
 mkdir -p logs/codehash
 
 # Find all .sol files in the 'out' directory
-find out -type f -name '*.json' | while read -r fileIn; do
+find out -type f -name '*.json' | while read -r file_in; do
   # Extract contract directory and JSON file name
-  contractDir=$(dirname "$fileIn" | sed 's|out/||')
-  jsonFile=$(basename "$fileIn")
+  contract_dir=$(dirname "$file_in" | sed 's|out/||')
+  json_file=$(basename "$file_in")
 
   # Skip if folder ends with .s.sol or .t.sol and not end with .sol
-  if [[ $contractDir == *".s.sol" ]] || [[ $contractDir == *".t.sol" ]] || [[ $contractDir != *".sol" ]]; then
+  if [[ $contract_dir == *".s.sol" ]] || [[ $contract_dir == *".t.sol" ]] || [[ $contract_dir != *".sol" ]]; then
     continue
   fi
 
   # Skip if the file is not a valid contract
-  compilationTarget=$(jq -r '.metadata.settings.compilationTarget' "$fileIn")
-  if echo "$compilationTarget" | jq -e 'keys[]' | grep -qE 'script|test|mock|dependencies|lib|libraries'; then
+  compilation_target=$(jq -r '.metadata.settings.compilationTarget' "$file_in")
+  if echo "$compilation_target" | jq -e 'keys[]' | grep -qE 'script|test|mock|dependencies|lib|libraries'; then
     continue
   fi
 
   # Skip if methodIdentifiers is empty
-  extFn=$(jq -r '.methodIdentifiers' "$fileIn")
-  if [ -z "$extFn" ] || [ "$extFn" == "{}" ]; then
+  ext_fn=$(jq -r '.methodIdentifiers' "$file_in")
+  if [ -z "$ext_fn" ] || [ "$ext_fn" == "{}" ]; then
     continue
   fi
 
   # Skip if deployedBytecode is empty (abstract contracts)
-  deployedBytecode=$(jq -r '.deployedBytecode.object' "$fileIn")
-  if [ "$deployedBytecode" == "0x" ]; then
+  deployed_bytecode=$(jq -r '.deployedBytecode.object' "$file_in")
+  if [ "$deployed_bytecode" == "0x" ]; then
     continue
   fi
 
-  # Calculate codehash and save to file
-  codehash=$(cast keccak "$deployedBytecode" 2>/dev/null || echo "Error")
-  if [ "$codehash" == "Error" ]; then
-    echo "Error: Failed to calculate codehash for $contractDir/$jsonFile"
-    continue
-  fi
+  nullified_metadata_deployed_bytecode=$(node -e "
+      const deployedBytecode = process.argv[1];
+      // Extract the last 2 bytes of the deployed bytecode
+      const metadataLength = parseInt(deployedBytecode.slice(-4), 16);
+      // Calculate the length of the metadata
+      const sliceLength = deployedBytecode.length - (metadataLength * 2 + 4);
+      // Create the nullified bytecode by replacing the metadata with zeros
+      const nullifiedBytecode = deployedBytecode.slice(0, sliceLength) + '0'.repeat(metadataLength * 2 + 4);
+      console.log(nullifiedBytecode);
+  " "$deployed_bytecode")
 
-  echo "$codehash" >"logs/codehash/${contractDir}:${jsonFile%.json}.log"
-  node .husky/storage-logger.js $fileIn "logs/storage/${contractDir}:${jsonFile%.json}.log" &
+  # Calculate codehash
+  codehash=$(cast keccak256 "$nullified_metadata_deployed_bytecode" 2>/dev/null)
+
+  echo "local: $codehash" >"logs/codehash/${contract_dir}:${json_file%.json}.log"
+  node .husky/storage-logger.js $file_in "logs/storage/${contract_dir}:${json_file%.json}.log" &
 done
 
 wait
